6c9b689cbf22961cc41800dba7275d284a9111b5
Dinosaur.YH <zoo.to@qq.com>
2023-07-18 22:31:33 +0800

    Updated homepage style.

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,6 +2,11 @@
 </script>
 <template>
   <div class="index">
+    <div class="introduce">
+      <h1 class="p1">DrawVictory Team</h1>
+      <p class="p2">绘赢团队是一个技术型团队，致力于程序开发，我们相信技术是第一生产力。</p>
+  <div class="background-img"></div>
+    </div>
   </div>
 </template>
 <style scoped lang="less">
